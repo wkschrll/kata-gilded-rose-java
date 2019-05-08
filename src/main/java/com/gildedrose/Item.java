42e7151e39ae1b4033df22c7c3984247f3af85e2
modernSE Trainee 3 <keydon+mse-trainee@gmail.com>
2019-05-08 12:26:52 +0200

    Ergebnis der Gruppe 3

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,8 +14,41 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + quality;
+		result = prime * result + sellIn;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (quality != other.quality)
+			return false;
+		if (sellIn != other.sellIn)
+			return false;
+		return true;
+	}
+   
+   
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,27 +2,45 @@ package com.gildedrose;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GildedRoseTest {
-
+	
     @Test
-    public void doTestSomeThing() {
-        Item brie = new Item("Aged Brie", 20, 50);
-        List<Item> items = new ArrayList<>();
-        for (int i = 0; i <= 100; i++) {
-            Item numberedBrie = new Item("Aged Brie", 20, i);
-            items.add(numberedBrie);
-        }
+    public void goldenMasterTest() {
+    	List<String> names = Arrays.asList("Aged Brie", "Alex", "Sulfuras, Hand of Ragnaros", 
+    			"Backstage passes to a TAFKAL80ETC concert", "");
+    	
+        List<Item> itemsOriginal = createTestData(names, -1, 100, -1, 15);
+        List<Item> itemsRefactored = createTestData(names, -1, 100, -1, 15);
 
-        GildedRose gildedRose = new GildedRose(items.toArray(new Item[]{}));
+        GildedRose gildedRose = new GildedRose(itemsOriginal.toArray(new Item[]{}));
+        GildedRoseRefactored alexGildedRose = new GildedRoseRefactored(itemsRefactored.toArray(new Item[]{}));
 
-        for (int i = 0; i < 22; i++) {
+        for (int i = 0; i < 20; i++) {
             gildedRose.updateQuality();
-            items.stream().map(Item::toString).forEach(System.out::println);
+            alexGildedRose.updateQuality();
+            
+            for (int j = 0; j < itemsOriginal.size(); j++) {
+            	assertEquals(itemsOriginal.get(j), itemsRefactored.get(j));
+            }
         }
-
-
     }
+
+	private List<Item> createTestData(List<String> names, int lowerQuality, int upperQuality, int lowerSellIn, int upperSellIn) {
+	    List<Item> items = new ArrayList<>();
+	    for (String name : names) {
+	    	for (int quality = lowerQuality; quality <= upperQuality; quality++) {
+	    		for (int sellIn = lowerSellIn; sellIn <= upperSellIn; sellIn++) {
+	    			 Item numberedBrie = new Item(name, quality, sellIn);
+	    		        items.add(numberedBrie);
+	    		}
+	    	}
+	    }
+		return items;
+	}
 }
