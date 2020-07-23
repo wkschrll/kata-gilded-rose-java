d1f57679fe69564cdcf072014595ffaf036d046c
chrisguter <christian.gutermann1337@gmail.com>
2020-07-23 13:07:27 +0000

    Gruppe #2 - GoldenRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,81 @@ class GildedRose {
     }
 
     public void updateQuality() {
+
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            switch (items[i].name) {
+                case "Aged Brie":
+                    updateBrie(items[i]);
+                    updateSellin(items[i]);
+                    break;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    updateBackstagePasses(items[i]);
+                    updateSellin(items[i]);
+                    break;
+
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+            
+                default:
+                    updateCommonItem(items[i]);
+                    updateSellin(items[i]);
+                    break;
+            }
+            
+        }
+    }
+
+    public void updateSellin(Item Item) {
+        Item.sellIn = Item.sellIn - 1;
+    }
+
+    public void updateCommonItem(Item commonItem) {
+
+        if (commonItem.quality > 0) {
+            commonItem.quality -= 1;
+        }
+
+        if (commonItem.sellIn < 0) { 
+            if (commonItem.quality > 0) {
+                commonItem.quality -= 1;
+            }
+        }
+
+    }
+
+    public void updateBrie(Item brie) {
+
+        if(brie.quality < 50) {
+            brie.quality += 1;
+        }
+
+        if (brie.sellIn <= 0 && brie.quality < 50) {
+            brie.quality += 1;
+        }
+    }
+
+    public void updateBackstagePasses(Item backstagePass) {
+
+        if(backstagePass.quality < 50) {
+            backstagePass.quality += 1;
+
+            if (backstagePass.sellIn < 11) {
+                if (backstagePass.quality < 50) {
+                    backstagePass.quality += 1;
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            if (backstagePass.sellIn < 6) {
+                if (backstagePass.quality < 50) {
+                    backstagePass.quality += 1;
                 }
             }
         }
-    }
+
+        if (backstagePass.sellIn <= 0) {
+            backstagePass.quality = backstagePass.quality - backstagePass.quality;
+        }
+    }   
 }
