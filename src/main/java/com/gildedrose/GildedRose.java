671ec33da1b0e2a80e7e1628294c4444c8470e6b
Marius Giebenhain <mariusgiebenhain@gmail.com>
2021-11-15 12:26:53 +0000

    Raum2 Solution

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.lang.reflect.Executable;
+
 class GildedRose {
     Item[] items;
 
@@ -7,56 +9,69 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    public void updateItems() {
+        for (Item item : items) {
+            updateItem(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void updateItem(Item item){
+        if(item.name.equals("Aged Brie")){
+            updateAgedBrie(item);
+            return;
+        }
+        if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            updateBackstage(item);
+            return;
+        }
+        if(item.name.equals("Sulfuras, Hand of Ragnaros")){
+            return;
+        }
+        updateStandardItem(item);
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+    private void updateStandardItem(Item item){
+        item.sellIn -= 1;
+        reduceQuality(item, 1);
+        if (item.sellIn < 0) {
+            reduceQuality(item, 1);
+        }
+    }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    private void updateAgedBrie(Item item){
+        item.sellIn -= 1;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality += 1;
             }
         }
     }
+
+    private void updateBackstage(Item item){
+        if (item.quality < 50) {
+            item.quality += 1;
+
+                if (item.sellIn < 11 && item.quality < 50) {
+                        item.quality = item.quality + 1;
+                }
+
+                if (item.sellIn < 6 && item.quality < 50) {
+                        item.quality = item.quality + 1;
+                } 
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void reduceQuality(Item item, int difference){
+        item.quality = Math.max(item.quality - difference, 0);
+    }
+
 }
