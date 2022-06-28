2841801b476f3dd2f45a0f42de9f04a46ff5e7c6
Avocado1012 <zhangjy92@gmail.com>
2022-06-28 12:30:33 +0000

    Group2

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Set;
+
 class GildedRose {
     Item[] items;
 
@@ -8,35 +10,22 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        Set<String> names = Set.of("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros");
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+            if (checkQuality(names, i)) {             
+                items[i].quality = items[i].quality - 1;
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (!checkNameNotEqual(i, Set.of("Backstage passes to a TAFKAL80ETC concert"))) {
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (checkNameNotEqual(i, Set.of("Sulfuras, Hand of Ragnaros"))) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
@@ -58,5 +47,13 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean checkQuality(Set<String> names, int i) {
+        return checkNameNotEqual(i, names) && items[i].quality > 0;
+    }
+
+    private boolean checkNameNotEqual(int i, Set<String> names) {
+        return !names.contains(items[i].name);
     }
 }
