65aabdb023f7a23419f24ed82029b75096d83f85
Jonathan Schenkenberger <jops97@proton.me>
2023-12-07 10:39:01 +0000

    Excercise GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,15 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
 class GildedRose {
-    Item[] items;
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+    private static final int QUALITY_THRESHOLD = 50;
+
+    private Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,54 +17,79 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            final Item item = items[i];
+            final String name = item.name;
+            int quality = prepareQuality(item);
+            final int sellIn = prepareSellIn(name, item.sellIn);
+            quality = finishCalculatingQuality(name, sellIn, quality);
+            item.quality = quality;
+            item.sellIn = sellIn;
+        }
+    }
+
+    private int prepareQuality(final Item item) {
+        final String name = item.name;
+        int quality = item.quality;
+        final int sellIn = item.sellIn;
+        if (name.equals(AGED_BRIE) || name.equals(BACKSTAGE)) {
+            if (checkQualityThreshold(quality)) {
+                quality++;
+
+                if (name.equals(BACKSTAGE) && checkThresholds(quality, sellIn, 11)) {
+                    quality++;
+                    if (checkThresholds(quality, sellIn, 6)) {
+                        quality++;
                     }
+                }
+            }
+        } else if (shouldDecreaseQuality(quality, name)) {
+            quality--;
+        } 
+        return quality;
+    }
+
+    private int prepareSellIn(final String name, final int initialSellInArg) {
+        int sellIn = initialSellInArg;
+        if (isNotEquals(name, SULFURAS)) {
+            sellIn--;
+        }
+        return sellIn;
+    }
+
+    private int finishCalculatingQuality(final String name, final int sellIn, final int qualityArg) {
+        int quality = qualityArg;
+        if (sellIn < 0) {
+            if (name.equals(AGED_BRIE)) {
+                if (checkQualityThreshold(quality)) {
+                    quality++;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                if (name.equals(BACKSTAGE)) {
+                    quality = 0;
+                } else if (shouldDecreaseQuality(quality, name)) {
+                    quality--;
                 }
             }
         }
+        return quality;
+    }
+
+    private boolean checkQualityThreshold(final int quality) {
+        return checkThresholds(quality, null, null);
+    }
+
+    private boolean checkThresholds(final int quality, final Integer sellIn, final Integer sellInThreshold) {
+        if (sellInThreshold != null) {
+            return quality < QUALITY_THRESHOLD && sellIn < sellInThreshold;
+        }
+        return quality < QUALITY_THRESHOLD;
+    }
+
+    private boolean shouldDecreaseQuality(final int quality, final String name) {
+        return quality > 0 && isNotEquals(name, SULFURAS);
+    }
+
+    private static boolean isNotEquals(Object a, Object b) {
+        return !Objects.equals(a, b);
     }
 }
